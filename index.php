7f5a1347c0b634f6e7bb9f6578c934cb81906b80
Aileen <ac952@cornell.edu>
2018-02-11 09:11:16 -0500

    minor change to nav

--- index.php
+++ index.php
@@ -44,13 +44,13 @@
       </figure>
     </div>
 
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
   </div>
